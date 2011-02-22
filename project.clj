6dd1a042a510f7f28d53fb7055a1c934bc580f99
ossareh <ossareh@gmail.com>
2011-02-22 00:46:50 -0800

    Pushing to clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject clj-riak "0.1.0-SNAPSHOT"
-  :description
-    "Clojure bindings to the Riak Protocol Buffers API."
-  :dependencies
-   [[org.clojure/clojure "1.2.0-RC2"]
-    [org.clojure/clojure-contrib "1.2.0-RC2"]
-    [org.clojars.mmcgrana/riak-java-pb-client "0.1.0-SNAPSHOT"]])
+  :description "Clojure bindings to the Riak Protocol Buffers API."
+
+  :repositories [["clojars" "http://clojars.org/repo"]]
+  
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojars.mmcgrana/riak-java-pb-client "0.1.0-SNAPSHOT"]]
+
+  :dev-dependencies [[lein-clojars "0.6.0"]])
