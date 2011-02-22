67e44a79849983b7c6b7331be03010fef20ee1e3
ossareh <ossareh@gmail.com>
2011-02-22 01:07:59 -0800

    Fixed project.clj - unfortunately this fork has to be pushed to clojars in its whole own group.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-riak "0.1.0-SNAPSHOT"
+(defproject org.clojars.ossareh/clj-riak "0.1.0-SNAPSHOT"
   :description "Clojure bindings to the Riak Protocol Buffers API."
 
-  :repositories [["clojars" "http://clojars.org/repo"]]
+  :repositories {"clojars" "http://clojars.org/repo"}
   
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
